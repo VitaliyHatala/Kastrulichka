a349414e8df4a3ccb9be455edf6a0c3932e80956
Danylo Dembitskyi <112881771+dembit429@users.noreply.github.com>
2024-02-26 21:00:29 +0200

    Update Header11.h
    Він тепер пустий

--- Header11.h
+++ Header11.h
@@ -1,16 +1,1 @@
-#include "Header1.h"
 
-class matrix2  {
-private:
-	int row;
-	int col;
-public:
-	matrix2();
-	matrix2(int r, int c);
-	matrix2(const matrix& m);
-	friend ostream& operator<<(ostream& os, const matrix2& m);
-	friend istream& operator>>(istream& is, matrix2& m);
-	bool operator+(istream& is, matrix2& m);
-	bool operator-(istream& is, matrix2& m);
-};
-void printMatrix(matrix2* m,int size);
