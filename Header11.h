259084ede9181d958869b834fd7e502d19c22cc7
Danylo Dembitskyi <112881771+dembit429@users.noreply.github.com>
2024-03-01 19:00:02 +0200

    Update Header11.h

--- Header11.h
+++ Header11.h
@@ -1,9 +1,7 @@
 #include "Header1.h"
 
-class matrix2 :public matrix {
-private:
-	int ones;
+class matrix2 : public matrix {
 public:
-
-
+    matrix2() : matrix() {}
+    matrix2(int r, int c, int** arr) : matrix(r, c, arr) {}
 };
