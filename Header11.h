8b0acc4ea18e539e3cadc360feb44096fa301969
nastmy <124741575+nastmy@users.noreply.github.com>
2024-03-03 22:40:29 +0200

    Update Header11.h
    NORMAL HEADER!!

--- Header11.h
+++ Header11.h
@@ -1,7 +1,12 @@
-#include "Header11.h"
+#pragma once
+#include "Header1.h"
+class matrix2 : public matrix 
+{
+private: 
+int element = 1;
+public:
+matrix2();
+matrix2(int r, int c, int** array); matrix2(matrix& m);
 
-matrix2::matrix2() : matrix() {}
-matrix2::matrix2(int r, int c, int** a) : matrix(r, c, a) {}
-matrix2::matrix2(matrix& m) : matrix(m) {}void matrix2::PrintM()
-{ cout << element << " ";
-}
+ void PrintM();
+};
