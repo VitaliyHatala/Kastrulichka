a7796cdf58571824edc995de7b8f8ddfb8750533
VitaliyHatala <161298762+VitaliyHatala@users.noreply.github.com>
2024-03-03 17:16:12 +0200

    Update Header11.h

--- Header11.h
+++ Header11.h
@@ -1,9 +1,10 @@
-#include "Header1.h"
+#pragma once
+#include "Header.h"
 
 class matrix2 : public matrix {
 public:
-    matrix2() : matrix() {}
-    matrix2(int r, int c, int** arr) : matrix(r, c, arr) {}
-
-    void fillWithOnes();
+   matrix2() : matrix() {}
+   matrix2(int r, int c, int** arr) : matrix(r, c, arr) {}
+   matrix2(matrix& m);
+   void fillWithOnes();
 };
