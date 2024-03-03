f68b44f87bbc28b7cf80f8154b5d3c422574be64
Danylo Dembitskyi <112881771+dembit429@users.noreply.github.com>
2024-03-03 19:03:07 +0200

    Header1.h
    Hopefully,final version of that.

--- Header1.h
+++ Header1.h
@@ -1,26 +1,23 @@
-#pragma once
-#include <fstream>
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-class matrix {
-protected:
-	int rows;
-	int cols;
-	int** array;
-public:
-	matrix();
-	matrix(int r, int c, int** array);
-	matrix(const matrix& m);
-	friend ostream& operator<<(ostream& os, const matrix& m);
-	friend istream& operator>>(istream& is, matrix& m);
-	bool operator+(matrix& m);
-	bool operator-(matrix& m);
-
-	int getRows() { return rows; }
-	int getCols() { return cols; }
+#pragma once 
+#include <fstream> 
+#include <iostream> 
+#include <string> 
+ 
+using namespace std; 
+ 
+class matrix { 
+protected: 
+ int rows; 
+ int cols; 
+ int** array; 
+public: 
+ matrix(); 
+ matrix(int r, int c, int** array); 
+ matrix(const matrix& m); 
+ friend ostream& operator<<(ostream& os, const matrix& m); 
+ friend istream& operator>>(istream& is, matrix& m); 
+ 
+ int getRows() { return rows; } 
+ int getCols() { return cols; } 
+ int getValue(int i, int j) { return array[i][j]; } 
 };
-
-void printMatrix(matrix* m, int size);
